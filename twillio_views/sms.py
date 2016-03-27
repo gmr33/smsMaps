337f380d93dd6314672a47a5f2c22dc4a4fd7e24
Gabriella Rojas <gmr33@cornell.edu>
2016-03-27 13:25:27 -0400

    handling bad to or from addresses now, telling people which addresses their text was matched to too

--- twillio_views/sms.py
+++ twillio_views/sms.py
@@ -23,18 +23,29 @@ def sms_request(request):
         to_address_query = get_keyword_arg(text, 'to').replace(' ','+')
         #also handle google api errors here.
         google_maps_response = requests.get('https://maps.googleapis.com/maps/api/directions/json?origin=' + from_address_query + '&destination=' + to_address_query + optional_mode_query + '&key=' + GOOGLE_API_KEY)
-        twiml_response = '<Response>'
-        steps = google_maps_response.json()['routes'][0]['legs'][0]['steps']
-        #consider making into 1 message for $$ saving.
-        for i in range(0,len(steps)):
-            instructions = strip_tags(steps[i]['html_instructions'])
-            distance = steps[i]['distance']['text']
-            duration = steps[i]['duration']['text']
-            twiml_response = twiml_response + '<Message>' + str(i+1) +'. ' + instructions + ' for '+ distance + ', ' + duration + '.' + '</Message>'
-            # becauase twillio's API is too slow and mis-orders the text responses sometimes.
-            time.sleep(.01)
-        twiml_response = twiml_response + '</Response>'
-        return HttpResponse(twiml_response, content_type='text/xml')
+        if google_maps_response.status_code == requests.codes.ok:
+            google_maps_json = google_maps_response.json()
+            if google_maps_json['geocoded_waypoints'][0]['geocoder_status'] != "OK":
+                return HttpResponse('<Response><Message>Unable to recognize your origin. Please revise the address and try again.</Message></Response>', content_type='text/xml')
+            if google_maps_json['geocoded_waypoints'][1]['geocoder_status'] != "OK":
+                return HttpResponse('<Response><Message>Unable to recognize your destination. Please revise the address and try again.</Message></Response>', content_type='text/xml')
+            origin = 'a'#google_maps_json['routes'][0]['legs']['start_address']
+            destination = 'b'#google_maps_json['routes'][0]['legs']['end_address']
+            twiml_response = '<Response><Message> Directions from ' + origin + ' to ' + destination + ' provided courtesy of Google Maps and the smsRouting project.</Message>'
+            steps = google_maps_json['routes'][0]['legs'][0]['steps']
+            #consider making into 1 message for $$ saving.
+            for i in range(0,len(steps)):
+                instructions = strip_tags(steps[i]['html_instructions'])
+                distance = steps[i]['distance']['text']
+                duration = steps[i]['duration']['text']
+                twiml_response = twiml_response + '<Message>' + str(i+1) +'. ' + instructions + ' for '+ distance + ', ' + duration + '.' + '</Message>'
+                # becauase twillio's API is too slow and mis-orders the text responses sometimes.
+                time.sleep(.01)
+            twiml_response = twiml_response + '</Response>'
+            return HttpResponse(twiml_response, content_type='text/xml')
+        else:
+            #handle specific error codes later
+            return HttpResponse('<Response><Message>Something has gone wrong with your request. Please try again.</Message></Response>', content_type='text/xml')
     else:
         return HttpResponse('<Response><Message> Please send valid message. All messages must be of the format from (origin address) to (destination address). Optionally you may inlude mode (type of directions) too.</Message></Response>', content_type='text/xml')
 
@@ -49,3 +60,6 @@ def valid_mode(text):
 
 def is_valid_request(text):
     return ('to' in text) and ('from' in text)
+
+
+
